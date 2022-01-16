9d22229f31283db0306e4c9697174985bd5b2719
andylukak <alexandru.tirim@yahoo.com>
2022-01-16 12:17:30 +0200

    Small commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import products from './assets/data/products.json';
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-
-	const [reviews, setReviews ] = useState([]);
-
+	const [reviews, setReviews] = useState([]);
+	console.log(products);
 	const fetchData = async () => {
-		const response = await fetch( 'http://localhost:3005/api/reviews', {
+		const response = await fetch('http://localhost:3005/api/reviews', {
 			mode: 'cors',
 			headers: {
 				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': '*'
-			}
+				'Access-Control-Allow-Origin': '*',
+			},
 		});
 		const data = await response.json();
 		setReviews(data);
-	}
+	};
 
-	useEffect( () => {
+	useEffect(() => {
 		fetchData();
 	}, []);
 
 	return (
 		<div className="App">
-			{reviews.map( (review) => {
-				return (
-					<div key={review.id}>
-						<h1> { review.movie_name} </h1>
-						<p> { review.movie_review }</p>
-					</div>
-				)
-			})}
+			<div className="grid">
+				{products.map((product, index) => {
+					return (
+						<div key={index} className="product-container">
+							<div className="product">
+								<div
+									className="product-image"
+									style={{
+										backgroundImage: `url(${product.image})`,
+									}}
+								/>
+								<div className="product-info">
+									<h1> {product.title} </h1>
+									<p> {product.price}</p>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const products =  require('../client/src/assets/data/products.json');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -13,12 +14,10 @@ const db = mysql.createConnection({
 
 const app = express();
 app.use(cors());
-app.use( bodyParser.urlencoded({ extended: false }) );
+app.use(bodyParser.urlencoded({ extended: false }));
 //Get all reviews
 app.get('/api/reviews', (req, res) => {
-	let sqlInsert =
-		'INSERT INTO movie_review (movie_name, movie_review) VALUES ("Terminator", "Terminator is a great movie")';
-	sqlInsert = "SELECT * FROM movie_review";
+	let sqlInsert = 'SELECT * FROM movie_review';
 	// Display all the data from the table
 	db.query(sqlInsert, (err, results) => {
 		if (err) {
@@ -39,47 +38,69 @@ app.get('/api/reviews/:id', (req, res) => {
 			res.send(results);
 		}
 	});
-} )
+});
 
 // Create a new review
 app.post('/api/reviews', (req, res) => {
-	let sqlInsert = 'INSERT INTO movie_review (movie_name, movie_review) VALUES (?, ?)';
-	db.query(sqlInsert, [req.body.movie_name, req.body.movie_review], (err, results) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(results);
+	let sqlInsert =
+		'INSERT INTO movie_review (movie_name, movie_review) VALUES (?, ?)';
+	db.query(
+		sqlInsert,
+		[req.body.movie_name, req.body.movie_review],
+		(err, results) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send(results);
+			}
 		}
-	});
+	);
 });
-
-// Delete a review
-app.delete('/api/reviews/:id', (req, res) => {
-	let sqlInsert = 'DELETE FROM movie_review WHERE id = ?';
-	db.query(sqlInsert, [req.params.id], (err, results) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(results);
-		}
-	});
-});
-
-// Update a review
-app.put('/api/reviews/:id', (req, res) => {
-	let sqlInsert = 'UPDATE movie_review SET movie_name = ?, movie_review = ? WHERE id = ?';
-	db.query(sqlInsert, [req.body.movie_name, req.body.movie_review, req.params.id], (err, results) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(results);
-		}
-	});
-});
-
-
 
 
 app.listen(3005, () => {
 	console.log('Server is running on port 3005');
 });
+
+const addRandomSizes = () => {
+
+	// Generate numbers between 138 and 170
+	let sizes = [];
+	for( let i = 0; i < 3; i++ ) {
+		sizes.push( Math.floor( Math.random() * ( 170 - 138 ) ) + 138 );
+	}
+	return sizes;
+
+}
+
+// Turn string price with $ into number
+const convertPrice = ( price ) => {
+	return parseFloat( price.replace( /\$/g, '' ) );
+}
+
+
+
+const addProductsToDB = ( products ) => {
+	let query = "INSERT INTO products (product_brand, product_title, product_description, product_image, product_price, product_sizes, product_category) VALUES  (?, ?, ?, ?, ?, ?, ?)";
+	products.map( ( product, index ) => {
+		if( product.sizes ) {
+		} else {
+			product.sizes = addRandomSizes( );
+		}
+		product.brand = product.title.split(' ')[0];
+		product.sizes = JSON.stringify( product.sizes );
+		product.price = convertPrice( product.price );
+		db.query( query, [product.brand, product.title, product.description, product.image, product.price, product.sizes, product.category ], ( err, results ) => {
+			if( err ) {
+				console.log( err );
+			} else {
+				console.log( results );
+			}
+		});
+	})
+
+}
+
+// addProductsToDB(products );
+
+
